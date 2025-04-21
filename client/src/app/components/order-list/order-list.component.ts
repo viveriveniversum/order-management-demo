07a456bd1f0c1be6cbef45e6fcb5adb56c066a67
Onur <onurmanap2461@gmail.com>
2025-04-21 22:07:49 +0200

    Export CSV

--- client/src/app/components/order-list/order-list.component.ts
+++ client/src/app/components/order-list/order-list.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
 import type { ColDef } from 'ag-grid-community';
-import { themeMaterial } from 'ag-grid-community';
 import { DialogDataButton } from '../dialog/dialog.component';
-import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { MatButtonModule } from '@angular/material/button';
+
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  GridReadyEvent,
+  themeMaterial,
+  GridApi,
+} from 'ag-grid-community';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { Order, OrderService } from '../../services/order.service';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -19,8 +27,22 @@ interface IRow {
 @Component({
   selector: 'app-order-list',
   standalone: true,
-  imports: [AgGridAngular],
+  imports: [AgGridAngular, MatIcon, MatIconModule, MatButtonModule],
   template: `
+    <div
+      class="csv-button"
+      style="display: flex; align-items: center; justify-content: flex-end; margin-bottom: 10px;"
+    >
+      <button
+        mat-fab
+        extended
+        (click)="onBtnExport()"
+        style="margin-right: 10px;  border: 1px solid rgb(84, 120, 198); color: aliceblue; border-radius: 10px; background-color: rgb(84, 120, 198);"
+      >
+        <mat-icon fontIcon="download"></mat-icon>
+        Export CSV
+      </button>
+    </div>
     <ag-grid-angular
       [theme]="theme"
       style="width: 100%; height: 500px;"
@@ -31,6 +53,7 @@ interface IRow {
       [paginationPageSize]="10"
       [rowHeight]="40"
       [headerHeight]="40"
+      (gridReady)="onGridReady($event)"
     />
   `,
 })
@@ -39,6 +62,7 @@ export class OrderListComponent implements OnInit {
   loading = false;
   error = '';
   public theme = themeMaterial;
+  private gridApi!: GridApi;
 
   colDefs: ColDef<IRow>[] = [
     { field: 'id', headerName: 'Order ID', cellRenderer: DialogDataButton },
@@ -88,5 +112,11 @@ export class OrderListComponent implements OnInit {
         this.loading = true;
       },
     });
+  }
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+  }
+  onBtnExport() {
+    this.gridApi.exportDataAsCsv();
   }
 }
