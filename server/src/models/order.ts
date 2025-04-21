a4ed0c9f0cc922599479e1867181ebfa8411a4d6
Onur <onurmanap2461@gmail.com>
2025-04-21 16:04:52 +0200

    Models - OrderItem toegevoegd

--- server/src/models/order.ts
+++ server/src/models/order.ts
@@ -3,5 +3,14 @@ export interface Order {
   customer: string;
   orderDate: string;
   status: "pending" | "processing" | "shipped" | "delivered" | "cancelled";
+  items?: OrderItem[] | null;
   totalAmount: number;
+}
+
+export interface OrderItem {
+  id: string;
+  productName: string;
+  quantity: number;
+  price: number;
+  subtotal: number;
 }
