52227c5613e81a23db2f62befe3eb7d6ffeaa842
Onur <onurmanap2461@gmail.com>
2025-04-22 15:06:14 +0200

    post method

--- server/src/routes/orders.ts
+++ server/src/routes/orders.ts
@@ -9,4 +9,7 @@ router.get("/", orderController.getAllOrders);
 // GET order by ID
 router.get("/:id", orderController.getOrderById);
 
+// POST create a new order
+router.post("/", orderController.createOrder);
+
 export default router;
