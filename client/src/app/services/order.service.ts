5bae6a89d1612bb36a7e832d8b606c3ca06c39ab
Onur <onurmanap2461@gmail.com>
2025-04-23 22:13:08 +0200

    Environment-specifieke instellingen - Angular

--- client/src/app/services/order.service.ts
+++ client/src/app/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface OrderItem {
   id: string;
@@ -23,7 +24,7 @@ export interface Order {
   providedIn: 'root',
 })
 export class OrderService {
-  private apiUrl = 'http://localhost:3000/orders';
+  private apiUrl = `${environment.apiUrl}/orders`;
 
   constructor(private http: HttpClient) {}
 
